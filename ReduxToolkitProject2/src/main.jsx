c40d7fd40dc53e90b920ae00975feb2b2596477f
Furkan Soylu <furkansylu97@gmail.com>
2025-06-15 21:40:33 +0300

    Store and Slice integrations were performed

--- ReduxToolkitProject2/src/main.jsx
+++ ReduxToolkitProject2/src/main.jsx
@@ -1,9 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'normalize.css';
-import 'bulma/css/bulma.min.css';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "normalize.css";
+import "bulma/css/bulma.min.css";
+import { store } from "./store/Store.jsx";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-)
+  </Provider>
+);
