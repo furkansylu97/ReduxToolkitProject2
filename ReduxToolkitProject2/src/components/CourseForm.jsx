a637b49277aba55e4c04901c2fe2dc426744ee47
Furkan Soylu <furkansylu97@gmail.com>
2025-06-16 00:26:16 +0300

    CourseForm component contents and css edits were performed

--- ReduxToolkitProject2/src/components/CourseForm.jsx
+++ ReduxToolkitProject2/src/components/CourseForm.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 function CourseForm() {
   return (
-    <div>CourseForm</div>
-  )
+    <div className="courseFormWrapper panel">
+      <h4 className="subtitle is-4">Add Course</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input className="input" />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Description</label>
+            <div className="control">
+              <textarea className="textarea" rows={1}></textarea>
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Price</label>
+            <div className="control">
+              <input className="input is-expanded" type="number" />
+            </div>
+          </div>
+        </div>
+        <div className="field">
+          <button className="button is-primary">Save</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default CourseForm
+export default CourseForm;
