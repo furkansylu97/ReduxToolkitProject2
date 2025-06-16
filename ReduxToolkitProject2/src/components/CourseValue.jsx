e46f89b27bcdec985f99da7a70342083c8f05a5b
Furkan Soylu <furkansylu97@gmail.com>
2025-06-16 17:12:38 +0300

    The calculation algorithm was edited

--- ReduxToolkitProject2/src/components/CourseValue.jsx
+++ ReduxToolkitProject2/src/components/CourseValue.jsx
@@ -2,22 +2,12 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function CourseValue() {
-  const totalCost = useSelector(({ form, courses: { data, searchTerm } }) => {
-    const filteredCourses = data.filter((course) =>
+  const totalCost = useSelector(({ form, courses: { data, searchTerm } }) => data.filter((course) =>
       course.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    
-    let cost = 0;
+    ).reduce((acc, course) => acc+course.cost,0)
+  );
 
-    for (let course of filteredCourses) {
-      cost += course.cost
-    }
-
-    return cost;
-
-  });
-
-  return <div>Total Cost: {totalCost} ₺</div>;
+  return <div className="costPrice">Total Cost: {totalCost} ₺</div>;
 }
 
 export default CourseValue;
