26ef4ac4e81747323b3191e0311a45385cc3b51f
Furkan Soylu <furkansylu97@gmail.com>
2025-06-16 14:28:53 +0300

    Course listing process was performed

--- ReduxToolkitProject2/src/components/CourseList.jsx
+++ ReduxToolkitProject2/src/components/CourseList.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 function CourseList() {
+  const courses = useSelector((state) => {
+    return state.courses.data;
+  });
+
+  const renderedCourse = courses.map((course) => {
+    return (
+      <div key={course.id} className="panel">
+        <h1>{course.name}</h1>
+        <p>{course.description}</p>
+        <p>{course.cost}</p>
+        <button className="button is-danger">Delete</button>
+      </div>
+    );
+  });
+
   return (
-    <div>CourseList</div>
-  )
+    <>
+      <div className="courseList">
+        {renderedCourse}
+      </div>
+    </>
+  );
 }
 
-export default CourseList
+export default CourseList;
