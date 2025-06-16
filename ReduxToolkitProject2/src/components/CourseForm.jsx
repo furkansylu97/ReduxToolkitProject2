e4c50f44eb60251cfe64cf11123c28677503a341
Furkan Soylu <furkansylu97@gmail.com>
2025-06-16 13:09:49 +0300

    The updated values ​​printing process were performed

--- ReduxToolkitProject2/src/components/CourseForm.jsx
+++ ReduxToolkitProject2/src/components/CourseForm.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
-import { changeName, changeDescription, changeCost  } from "../store/slices/FormSlice";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  changeName,
+  changeDescription,
+  changeCost,
+} from "../store/slices/FormSlice";
 
 function CourseForm() {
+  const name = useSelector((state) => state.form.name);
+  const description = useSelector((state) => state.form.description);
+  const cost = useSelector((state) => state.form.cost);
 
-const name = useSelector((state) => state.form.name);
-const description = useSelector((state) => state.form.description);
-const cost = useSelector((state) => state.form.cost);
+  const dispatch = useDispatch();
 
-const dispatch = useDispatch();
-
-console.log(name, description, cost);
+  console.log(name, description, cost);
 
   return (
     <div className="courseFormWrapper panel">
@@ -20,19 +23,39 @@ console.log(name, description, cost);
           <div className="field">
             <label className="label">Name</label>
             <div className="control">
-              <input className="input" onChange={(event) => { dispatch(changeName(event.target.value)) }} />
+              <input
+                className="input"
+                onChange={(event) => {
+                  dispatch(changeName(event.target.value));
+                }}
+                value={name}
+              />
             </div>
           </div>
           <div className="field">
             <label className="label">Description</label>
             <div className="control">
-              <textarea className="textarea" rows={1} onChange={(event) => { dispatch(changeDescription(event.target.value)) }}></textarea>
+              <textarea
+                className="textarea"
+                rows={1}
+                onChange={(event) => {
+                  dispatch(changeDescription(event.target.value));
+                }}
+                value={description}
+              ></textarea>
             </div>
           </div>
           <div className="field">
             <label className="label">Price</label>
             <div className="control">
-              <input className="input is-expanded" type="number" onChange={(event) => { dispatch(changeCost(parseInt(event.target.value))) }} />
+              <input
+                className="input is-expanded"
+                type="number"
+                onChange={(event) => {
+                  dispatch(changeCost(parseInt(event.target.value)));
+                }}
+                value={cost}
+              />
             </div>
           </div>
         </div>
